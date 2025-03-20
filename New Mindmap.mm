c99e6124d6f8beaa8a1d2380fce3d5050f5730d6
Tae Deok Kweon <tdkweon@wsu.ac.kr>
2025-03-20 09:33:29 +0900

    Add files via upload

---
+++ New Mindmap.mm
@@ -0,0 +1,109 @@
+<map version="freeplane 1.9.13">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node LOCALIZED_TEXT="new_mindmap" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1606856177697"><hook NAME="MapStyle" background="#2e3440">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" fit_to_viewport="false" associatedTemplateLocation="template:/dark_nord_template.mm"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_671184412" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#484747" BACKGROUND_COLOR="#eceff4" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#88c0d0" WIDTH="2" TRANSPARENCY="255" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_671184412" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#81a1c1" WIDTH="3" DASH="SOLID"/>
+<richcontent CONTENT-TYPE="plain/auto" TYPE="DETAILS"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" BORDER_WIDTH="1.9 px">
+<edge STYLE="bezier" COLOR="#81a1c1" WIDTH="3"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes" COLOR="#eceff4">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ebcb8b">
+<icon BUILTIN="clock2"/>
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating" COLOR="#484747">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#e5e9f0" BACKGROUND_COLOR="#5e81ac" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#5e81ac"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_779275544" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#bf616a">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#bf616a" TRANSPARENCY="255" DESTINATION="ID_779275544"/>
+<font SIZE="14"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#484747" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font NAME="Ubuntu" SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#eceff4" BACKGROUND_COLOR="#d08770" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt">
+<font NAME="Ubuntu" SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#3b4252" BACKGROUND_COLOR="#ebcb8b">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#2e3440" BACKGROUND_COLOR="#a3be8c">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#2e3440" BACKGROUND_COLOR="#b48ead">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" BACKGROUND_COLOR="#81a1c1">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BACKGROUND_COLOR="#88c0d0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BACKGROUND_COLOR="#8fbcbb">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BACKGROUND_COLOR="#d8dee9">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BACKGROUND_COLOR="#e5e9f0">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BACKGROUND_COLOR="#eceff4">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font BOLD="true"/>
+<node TEXT="HW 1" FOLDED="true" POSITION="right" ID="ID_1283570832" CREATED="1662339070717" MODIFIED="1662339182623">
+<node TEXT="Name" ID="ID_686484043" CREATED="1662339188967" MODIFIED="1662339225470">
+<node TEXT="Sunjip Yim" ID="ID_963320785" CREATED="1662339259992" MODIFIED="1662339263864"/>
+</node>
+<node TEXT="From where" ID="ID_1546835260" CREATED="1662339229819" MODIFIED="1662339236113">
+<node TEXT="Native Korean" ID="ID_477780489" CREATED="1662339265762" MODIFIED="1662339269925"/>
+</node>
+<node TEXT="Three #s I like" ID="ID_1265619951" CREATED="1662339236612" MODIFIED="1662339248237">
+<node TEXT="1. Japanes Cycling Manga" ID="ID_1916714593" CREATED="1662339278331" MODIFIED="1662339299075"/>
+<node TEXT="2. Black Pink" ID="ID_1122778425" CREATED="1662339300356" MODIFIED="1662339321898"/>
+<node TEXT="3. Haruki Murakami" ID="ID_1469258919" CREATED="1662339324046" MODIFIED="1662339334917"/>
+</node>
+</node>
+<node TEXT="" POSITION="left" ID="ID_641929344" CREATED="1662339109344" MODIFIED="1662339109344"/>
+<node TEXT="" POSITION="left" ID="ID_1515634300" CREATED="1662339151561" MODIFIED="1662339151561"/>
+<node TEXT="" POSITION="left" ID="ID_1758850992" CREATED="1662339152574" MODIFIED="1662339152574"/>
+<node TEXT="" POSITION="left" ID="ID_301073804" CREATED="1662339153461" MODIFIED="1662339153461"/>
+<node TEXT="HW2" POSITION="right" ID="ID_303479491" CREATED="1662339123808" MODIFIED="1662339472691">
+<node TEXT="" ID="ID_1216320703" CREATED="1662339195752" MODIFIED="1662339525345">
+<hook URI="../../../../../../Users/s2-302-01/Desktop/Unknown.jpeg" SIZE="1.0" NAME="ExternalObject"/>
+</node>
+</node>
+<node TEXT="HW3" POSITION="right" ID="ID_539375796" CREATED="1662339141548" MODIFIED="1662339750752">
+<node TEXT="" ID="ID_429161347" CREATED="1662339198587" MODIFIED="1662339764125">
+<hook URI="Unknown.jpeg" SIZE="1.0" NAME="ExternalObject"/>
+</node>
+</node>
+<node TEXT="" POSITION="right" ID="ID_1255142809" CREATED="1662339145419" MODIFIED="1662339145419"/>
+</node>
+</map>
