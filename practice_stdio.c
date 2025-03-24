9f425026246951c2416e7507bd287533cf2938ed
Tae Deok Kweon <tdkweon@wsu.ac.kr>
2025-03-24 16:30:01 +0900

    Update practice_stdio.c

--- practice_stdio.c
+++ practice_stdio.c
@@ -13,18 +13,45 @@ int main(){
     printf("\"Hello world\"\n");
 
 
-    int age = 25;
-    float pi = 3.14;
-    printf("Age: %d, PI: %.2f\n", age, pi);
+   int age = 25;
+   float pi = 3.1412345;
+   printf("Age: %d, PI: %.2f\n", age, pi);
 
-    int count = printf("Hello");
-    printf("Characters printed: %d\n", count);
+   int count = printf("12345\n");
+   printf("\nCharacters: %d\n", count);
 
-    int count2 = printf("Hello\n");
-    printf("Characters printed: %d\n", count2);
+   puts("Hello, World"); //automatically appends a newline (\n) at the end
 
 
-    /* puts */
+   putchar('A');
+   putchar('B');
+   putchar('C');
+   printf("====\n");
+
+   // ======================================
+
+   printf("---------------------------------------\n");
+
+   int age2;
+   float height;
+   printf("Enter your age and height:");
+   scanf("%d %f", &age, &height);
+   printf("You are %d years old and %.2f meters tall.\n", age, height);
+
+   char ch;
+   printf("Enter a character: ");
+   ch = getchar();
+   printf("You entered: %c\n", ch);
+
+
+   char name[50];
+   printf("Enter your name: ");
+   // fgets reads a whole line from the input (up to n-1 characters). 
+   // It includes spaces and stops at a newline (\n). 
+   // Adds a newline character (\n) if the user presses Enter. 
+   fgets(name, sizeof(name), stdin);
+   printf("Hello, %s", name);
+
     
 
     return 0;
