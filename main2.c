9c779d40272daf98a0aed3bee558ed0db5d9bae6
Tae Deok Kweon <tdkweon@wsu.ac.kr>
2025-09-10 10:52:03 +0900

    Create main2.c

---
+++ main2.c
@@ -0,0 +1,10 @@
+// ID: 28321y8239y4
+// Name: kelfwe nfgwerl
+
+#include <stdio.h>
+
+int main(void) {
+    // Write your code here to display "Hello, world!"
+    printf("hello, world\n");
+    return 0;
+}
